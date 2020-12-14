ff58997c5a67b6cab65db912b109ba2cbf64a4ab
kalemena <kalemena@free.fr>
2020-12-14 21:42:05 +0100

    Diagrams: icons

--- images/diagrams/diagrams-icons.py
+++ images/diagrams/diagrams-icons.py
@@ -2,9 +2,23 @@
 
 import os
 
-githubrootpath="![](https://github.com/mingrammer/diagrams/blob/master/"
+githubrootpath="https://github.com/mingrammer/diagrams/blob/master/"
 
-print("# List of icons")
+print("""
+= List of icons
+
+.Icons
+|===
+| Name | Icon
+
+""")
+
 for root, dirs, files in os.walk("resources", topdown=False):
     for name in files:
-        print("Name: ", root[10:] + name[:-4], "icon: ", os.path.join(githubrootpath, root, name, "?raw=true)"))
+        print("| %s " % (root[10:] + name[:-4]))
+        print("| image:%s[]" % (os.path.join(githubrootpath, root, name, "?raw=true")))
+        print("\n")
+
+print("""
+|===
+""")
