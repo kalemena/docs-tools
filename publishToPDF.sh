559a6a9de5e4a0874e44aa88404e1f1acbcd7592
kalemena <kalemena@free.fr>
2021-01-03 21:33:16 +0100

    1st PDF build

--- publishToPDF.sh
+++ publishToPDF.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-rm -rf `pwd`/build
-cp -r `pwd`/docs-sample `pwd`/build
+echo `date "+%Y-%m-%d %H:%M:%S"`
+source ./publishAssets.sh
 
-docker run --rm -it \
-    -v `pwd`/build:/docs \
-    kalemena/asciidoc:0.4.0 \
-    asciidoctor-pdf -a allow-uri-read -a icons=font -r asciidoctor-diagram -D /docs/ -B /docs /docs/_pdfbook.adoc
+REVISION=${REVISION:-latest}
+
+for FILE in ${PATH_PROJECT}/build/_*-Book.adoc; do 
+    echo "===="
+    FILENAME=$(/usr/bin/basename "${FILE}")
+    echo "Publishing to PDF ... ${FILENAME}"
+
+    docker run ${DOCKER_USERID} --rm --name docsbuild \
+        -v ${PATH_PROJECT}:/project \
+        asciidoctor/docker-asciidoctor \
+        asciidoctor-pdf -a allow-uri-read -a icons=font -r asciidoctor-diagram -D /project/build/ -B /project/build /project/build/${FILENAME}
+    # -a pdf-style=/docs/themes/advanced-theme.yml
+    echo "===="
+done
+
+echo `date "+%Y-%m-%d %H:%M:%S"`
